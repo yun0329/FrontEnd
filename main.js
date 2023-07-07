740e853c61d2d1531ede5a1b69e3a6b7c05d9e00
JANGJEEYEON <jjiyeon27@gmail.com>
2023-07-07 14:08:12 +0900

    리뷰 작성 페이지

--- main.js
+++ main.js
@@ -5,19 +5,6 @@ document
     const reader = new FileReader();
 
     reader.onload = function (event) {
-      imgSrc = event.target.result;
-    };
-
-    reader.readAsDataURL(file);
-  });
-
-document
-  .getElementById("input_image")
-  .addEventListener("change", function (event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-
-    reader.onload = function (event) {
       const imgSrc = event.target.result;
       document.getElementById("image_preview").src = imgSrc;
       document.getElementById("image_preview").style.display = "block";
